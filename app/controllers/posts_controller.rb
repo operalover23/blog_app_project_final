92ae600326d266d34d0ef9696dded2c71ae4e32d
Elizabeth <rock09@email.franklin.edu>
2019-04-08 16:15:10 -0500

    reduced redundancies

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:edit, :update, :show, :destroy]
+
   def index
     @posts = Post.all
   end
@@ -8,7 +10,7 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
+
   end
 
   def create
@@ -22,7 +24,7 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post = Post.find(params[:id])
+
     if @post.update(post_params)
       flash[:notice] = "Blog updated successfully"
       redirect_to post_path(@post)
@@ -32,17 +34,20 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     flash[:notice] = "Blog successfully deleted"
     redirect_to posts_path
   end
 
   private
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
     def post_params
       params.require(:post).permit(:title, :description)
     end
