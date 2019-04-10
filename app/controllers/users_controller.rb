e8c6cd4043734f51f270b0afc07ef09bc8fa234c
Elizabeth <rock09@email.franklin.edu>
2019-04-10 18:15:39 -0500

    updated edit cviews

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < Application UsersController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to my blog #{@user.username}"
+      redirect_to posts_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Account updated successfully"
+      redirect_to posts_path
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
+end
