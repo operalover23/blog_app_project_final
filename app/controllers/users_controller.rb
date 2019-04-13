d9ce996607c095c8f0ac1e76acbf96ba03d3dea9
Elizabeth <rock09@email.franklin.edu>
2019-04-13 13:57:31 -0500

    changed styles and fixed bugs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
     if @user.save
       session[:user_id] = @user.id
       flash[:success] = "Welcome to my blog #{@user.username}"
-      redirect_to user_path
+      redirect_to users_path
     else
       render 'new'
     end
